5edb97b5237079dc04f9475ee1e187a4a709bcf8
Amber Williams <awilliams1234@live.com>
2020-02-15 03:29:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-elsif phrase != phrase.upcase
+  elsif phrase != phrase.upcase
   "HUH?! SPEAK UP SONNY!"
-else
-  'NO, NOT SINCE 1983!'
 end
